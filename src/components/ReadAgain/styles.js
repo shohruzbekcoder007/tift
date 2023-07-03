9dbdde972a63ff3a8fe584ffc1c6fe118b29febc
shohruzbekcoder007 <shohruzbekcoder007@gmail.com>
2023-07-03 09:52:10 -0700

    read again

--- src/components/ReadAgain/styles.js
+++ src/components/ReadAgain/styles.js
@@ -1,1 +1,17 @@
 import styled from 'styled-components';
+
+export const ReadAgainWrapper = styled.div`
+    width: 100%;
+    display: flex;
+    gap: 18px;
+    align-items: start;
+    justify-content: space-between;
+`
+
+export const ReadAgainLeft = styled.div`
+    width: 730px;
+`
+
+export const ReadAgainReight = styled.div`
+    width: 356px;
+`
