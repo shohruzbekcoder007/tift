9608c0e8ba7fcce66f92895e37ffc690ad539518
Очилов Ш. Ш <s.ochilov@stat.local>
2023-07-11 17:42:33 +0500

    ThematicBlock finished 17:42 11.07.2023

--- src/components/TeacherSciences/ThematicBlock/index.jsx
+++ src/components/TeacherSciences/ThematicBlock/index.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
-import { ThematicBlockText, ThematicBlockWrapper } from './styled'
+import { ThematicBlockBox, ThematicBlockBoxBody, ThematicBlockBoxHeader, ThematicBlockBoxs, ThematicBlockText, ThematicBlockWrapper } from './styled'
 
 export default function ThematicBlock() {
   return (
     <ThematicBlockWrapper>
         <ThematicBlockText>Тематики блока</ThematicBlockText>
+        <ThematicBlockBoxs>
+            <ThematicBlockBox>
+                <ThematicBlockBoxHeader>#     SPM201-1-29-05-2023</ThematicBlockBoxHeader>
+                <ThematicBlockBoxBody>1      Ahmedov Ulug‘bek Zarifboy o‘g‘li (Программный инжиниринг - 321-19 DIr)</ThematicBlockBoxBody>
+            </ThematicBlockBox>
+        </ThematicBlockBoxs>
     </ThematicBlockWrapper>
   )
 }
--- src/components/TeacherSciences/ThematicBlock/styled.js
+++ src/components/TeacherSciences/ThematicBlock/styled.js
@@ -10,4 +10,40 @@ export const ThematicBlockText = styled.h2`
     font-size: ${props => props.theme.text.H2.font_size};
     line-height: normal;
     font-style: normal;
+    margin-bottom: 16px;
+`
+
+export const ThematicBlockBoxs = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: flex-start;
+    gap: 18px
+`
+
+export const ThematicBlockBox = styled.div`
+    background-color: #fff;
+    padding: 12px 10px;
+    border-radius: 10px;
+    display: inline-block;
+    max-width: 356px;
+`
+
+export const ThematicBlockBoxHeader = styled.div`
+    padding: 16px;
+    border-bottom: 1px solid #EEE;
+    h5 {
+        font-weight: ${props => props.theme.text.subtitle1.font_weight};
+        font-size: ${props => props.theme.text.subtitle1.font_size};
+        color: #000;
+    }
+`
+
+export const ThematicBlockBoxBody = styled.div`
+    padding: 16px;
+    p {
+        font-weight: ${props => props.theme.text.subtitle2.font_weight};
+        font-size: ${props => props.theme.text.subtitle2.font_size};
+        color: ${props => props.theme.color.secondary_text_color};
+        line-height: 150%;
+    }
 `
