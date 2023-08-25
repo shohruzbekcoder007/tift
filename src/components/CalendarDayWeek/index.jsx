773d9671966601185fcb1500f0dc72556bacc544
Nodirbek9790 <jonibekyorkulov@gmail.com>
2023-08-25 15:17:30 +0500

    25.08.2023 15:17

--- src/components/CalendarDayWeek/index.jsx
+++ src/components/CalendarDayWeek/index.jsx
@@ -8,25 +8,32 @@ import { Paper } from '@mui/material';
 const events = [ 
   { 
     id: 1, 
-    title: 'event 1', 
-    start: '2023-08-09T10:45:00', 
-    end: '2023-08-09T12:00:00',
+    title: 'A101 proggamming1 - MTH-903',
+    start: '2023-08-25T10:45:00', 
+    end: '2023-08-25T12:00:00',
     backgroundColor: "green",
     allDay: false
   },
   { 
     id: 12, 
     title: 'event 1', 
-    start: '2023-07-07T10:45:00', 
-    end: '2023-07-07T12:00:00',
+    start: '2023-08-26T10:45:00', 
+    end: '2023-08-26T12:00:00',
     backgroundColor: "yellow",
     allDay: false
   },
   { 
     id: 2, 
     title: 'event 2', 
-    start: '2023-07-07T13:00:00', 
-    end: '2023-07-07T18:00:00', 
+    start: '2023-08-26T13:00:00', 
+    end: '2023-08-26T15:00:00', 
+    allDay: false
+  },
+  { 
+    id: 2, 
+    title: 'event 2', 
+    start: '2023-08-26T13:00:00', 
+    end: '2023-08-26T15:00:00', 
     allDay: false
   }
 ]; 
