9d1a161054881a884caa96189a2fdf1524fac4fa
shohruzbekcoder <shohruzbekcoder007@gmail.com>
2023-09-05 17:48:52 +0500

    schedule table style and effect

---
+++ src/components/AdminList/ScheduleStudy/styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const ScheduleTable = styled.th`
+    cursor: pointer;
+    :hover {
+        box-shadow: rgba(204, 204, 204, 0.29) 1px 1px 10px 10px;
+    }
+`
