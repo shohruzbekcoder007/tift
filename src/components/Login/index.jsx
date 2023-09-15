5ef90ccdcc1d47b7da16aca5386bb730973fcdf2
shohruzbekcoder <shohruzbekcoder007@gamil.com>
2023-09-15 20:32:16 +0500

    15.09.2023 30:32

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -153,68 +153,13 @@ import { Alert, Tooltip, Stack, Typography, Link, Box } from '@mui/material';
 // auth
 // layouts
 //
-import AuthLoginForm from '../auth/AuthLoginForm';
+// import AuthLoginForm from '../auth/AuthLoginForm';
 import LoginLayout from '../layouts/LoginLayout';
 import SignInSide from '../SignInSide';
 
 // ----------------------------------------------------------------------
 
 export default function Login() {
-
-  let navigate = useNavigate()
-  const dispatch = useDispatch()
-  const [pageLoading, setPageLoading] = useState(false)
-  const [openAlert, setOpenAlert] = useState(false)
-  const [haveatoken, setHaveatoken] = useState(false)
-
-  const handleClose = (event, reason) => {
-    if (reason === 'clickaway') {
-      return;
-    }
-
-    setOpenAlert(false);
-  };
-
-  const successfulFunctionGetToken = (response) => {
-    sessionStorage.setItem('access_token', response.data.access)
-    setHaveatoken(true)
-    getRole(user_me, successfulFunctionGetRole, errorFunctionGetRole)
-  }
-
-  const errorFunctionGetToken = (error) => {
-    // console.log({ errorMessage: error.toString() });
-    // console.error("There was an error!", error);
-    setPageLoading(false)
-    setOpenAlert(true)
-  }
-
-  const successfulFunctionGetRole = (response) => {
-    dispatch(setUser(response.data))
-    const user_role = getRoleUser(response.data)
-    setPageLoading(false)
-    if (user_role === "admin") {
-      navigate(`/${user_role}/users`)
-    }else {
-      navigate(`/${user_role}/dashboard`)
-    }
-  }
-
-  const errorFunctionGetRole = (error) => {
-    // console.log({ errorMessage: error.toString() });
-    // console.error("There was an error!", error);
-    setPageLoading(false)
-    setOpenAlert(true)
-  }
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    getToken(token_url, {
-      username: data.get('username'),
-      password: data.get('password'),
-    }, successfulFunctionGetToken, errorFunctionGetToken)
-    setPageLoading(true)
-  }
 
   return (
     <LoginLayout>
