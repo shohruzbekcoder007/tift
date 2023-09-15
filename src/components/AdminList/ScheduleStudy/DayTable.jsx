fadbf9ae9611e46919fa9413b56549e995f56cba
JaMeS-18-18 <chitak1818@gmail.com>
2023-09-15 09:50:30 +0500

    15.09.2023 9:50

--- src/components/AdminList/ScheduleStudy/DayTable.jsx
+++ src/components/AdminList/ScheduleStudy/DayTable.jsx
@@ -18,182 +18,184 @@ import { scheduletable } from '../../../utils/API_urls';
 import { Alert } from '@mui/material';
 
 const BootstrapDialog = styled(Dialog)(({ theme }) => ({
-    '& .MuiDialogContent-root': {
-        padding: theme.spacing(2),
-    },
-    '& .MuiDialogActions-root': {
-        padding: theme.spacing(1),
-    },
+  '& .MuiDialogContent-root': {
+    padding: theme.spacing(2),
+  },
+  '& .MuiDialogActions-root': {
+    padding: theme.spacing(1),
+  },
 }));
 
 export default function DayTable({ oneday, groups, day, para, room }) {
 
-    const [open, setOpen] = useState(false);
-    const [new_lessons, setNewLessons] = useState([])
-    const [error, setError] = useState(false)
-    const [data, setData] = useState({
-        group: groups[0]?.value,
-        weekday: getDay(day),
-        para: para,
-        room: room?.id,
-        types: lesson_types[0].value
+  const [open, setOpen] = useState(false);
+  const [new_lessons, setNewLessons] = useState([])
+  const [error, setError] = useState(false)
+  const [data, setData] = useState({
+    group: groups[0]?.value,
+    weekday: getDay(day),
+    para: para,
+    room: room?.id,
+    types: lesson_types[0].value
+  })
+
+  const weekDays = useMemo(() => {
+    return week_day.map(elem => {
+      return {
+        value: elem.value,
+        name: elem.uz
+      }
     })
+  }, [])
 
-    const weekDays = useMemo(() => {
-        return week_day.map(elem => {
-            return {
-                value: elem.value,
-                name: elem.uz
-            }
+  const lessonTypes = useMemo(() => {
+    return lesson_types.map(elem => {
+      return {
+        name: elem.uz,
+        value: elem.value
+      }
+    })
+  }, [])
+
+  const paraList = useMemo(() => {
+    return [
+      {
+        name: 1,
+        value: 1
+      },
+      {
+        name: 2,
+        value: 2
+      },
+      {
+        name: 3,
+        value: 3
+      },
+      {
+        name: 4,
+        value: 4
+      },
+      {
+        name: 5,
+        value: 5
+      },
+      {
+        name: 6,
+        value: 6
+      }
+    ]
+  }, [])
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleSubmit = () => {
+    createScheduleTable(scheduletable, data, response => {
+      if (response.id) {
+        setNewLessons(prev => {
+          return [...prev, {
+            types: data.types,
+            group: groups.find((el) => el.value == data.group)
+          }]
         })
-    }, [])
+        handleClose()
+      } else {
+        setError(true)
+      }
+    }, error => {
+      console.log(error)
+      setError(true)
+    })
+  }
 
-    const lessonTypes = useMemo(() => {
-        return lesson_types.map(elem => {
-            return {
-                name: elem.uz,
-                value: elem.value
-            }
-        })
-    }, [])
+  return (
+    <>
+      <ScheduleTable
+        onClick={() => {
+          handleClickOpen()
+        }}
+      >
+        {
+          oneday?.group?.map((element, indx) => {
+            return <span key={indx} style={{ padding: "10px", backgroundColor: "#eee", borderRadius: "10px", margin: "5px", display: "inline-block", color: "#000" }}>{element}</span>
+          })
+        }
+        {
+          new_lessons?.map((element, indx) => {
+            return <span key={indx} style={{ padding: "10px", backgroundColor: "#eee", borderRadius: "10px", margin: "5px", display: "inline-block", color: "#000" }}>{element.group.name}</span>
+          })
+        }
+      </ScheduleTable>
+      <BootstrapDialog
+        onClose={handleClose}
+        aria-labelledby="customized-dialog-title"
+        open={open}
+      >
+        <DialogTitle sx={{ m: 0, p: 2 }} id="customized-dialog-title">
+          Dars jadvaliga qo'shish
+        </DialogTitle>
 
-    const paraList = useMemo(() => {
-        return [
-            {
-                name: 1,
-                value: 1
-            },
-            {
-                name: 2,
-                value: 2
-            },
-            {
-                name: 3,
-                value: 3
-            },
-            {
-                name: 4,
-                value: 4
-            },
-            {
-                name: 5,
-                value: 5
-            },
-            {
-                name: 6,
-                value: 6
-            }
-        ]
-    }, [])
-
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
-
-    const handleClose = () => {
-        setOpen(false);
-    };
-
-    const handleSubmit = () => {
-        createScheduleTable(scheduletable, data, response => {
-            if(response.id){
-                setNewLessons(prev => { return [...prev, {
-                    types: data.types,
-                    group: groups.find((el) => el.value == data.group)
-                }]})
-                handleClose()
-            }else{
-                setError(true)
-            }
-        }, error => {
-            console.log(error)
-            setError(true)
-        })
-    }
-
-    return (
-        <>
-            <ScheduleTable
-                onClick={() => {
-                    handleClickOpen()
-                }}
+        <IconButton
+          aria-label="close"
+          onClick={handleClose}
+          sx={{
+            position: 'absolute',
+            right: 8,
+            top: 8,
+            color: (theme) => theme.palette.grey[500],
+          }}
+        >
+          <CloseIcon />
+        </IconButton>
+        <DialogContent dividers style={{ width: "450px" }}>
+          {error ? <Alert severity="success" color="error" sx={{ my: 1 }}>
+            Jadvalga dars qo'shib bo'lmadi
+          </Alert> : <></>}
+          <ModalSelectWrapper>
+            <Typography
+              id="keep-mounted-modal-title"
+              variant="h6"
+              component="h4"
+              sx={{
+                fontSize: "16px",
+                fontWeight: 600,
+                color: "#000",
+                mb: "10px"
+              }}
             >
-                {
-                    oneday?.group?.map((element, indx) => {
-                        return <span key={indx} style={{ padding: "10px", backgroundColor: "#eee", borderRadius: "10px", margin: "5px", display: "inline-block", color: "#000" }}>{element}</span>
-                    })
+              Group
+            </Typography>
+            <AllSelectFullWidth
+              chageValueFunction={val => setData(prev => {
+                return {
+                  ...prev,
+                  group: val
                 }
-                {
-                    new_lessons?.map((element, indx) => {
-                        return <span key={indx} style={{ padding: "10px", backgroundColor: "#eee", borderRadius: "10px", margin: "5px", display: "inline-block", color: "#000" }}>{element.group.name}</span>
-                    })
-                }
-            </ScheduleTable>
-            <BootstrapDialog
-                onClose={handleClose}
-                aria-labelledby="customized-dialog-title"
-                open={open}
+              })}
+              selectOptions={groups}
+            />
+          </ModalSelectWrapper>
+          <ModalSelectWrapper>
+            <Typography
+              id="keep-mounted-modal-title"
+              variant="h6"
+              component="h4"
+              sx={{
+                fontSize: "16px",
+                fontWeight: 600,
+                color: "#000",
+                mb: "10px"
+              }}
             >
-                <DialogTitle sx={{ m: 0, p: 2 }} id="customized-dialog-title">
-                    Dars jadvaliga qo'shish
-                </DialogTitle>
-                
-                <IconButton
-                    aria-label="close"
-                    onClick={handleClose}
-                    sx={{
-                        position: 'absolute',
-                        right: 8,
-                        top: 8,
-                        color: (theme) => theme.palette.grey[500],
-                    }}
-                >
-                    <CloseIcon />
-                </IconButton>
-                <DialogContent dividers style={{ width: "450px" }}>
-                    {error?<Alert severity="success" color="error" sx={{my: 1}}>
-                        Jadvalga dars qo'shib bo'lmadi
-                    </Alert>:<></>}
-                    <ModalSelectWrapper>
-                        <Typography
-                            id="keep-mounted-modal-title"
-                            variant="h6"
-                            component="h4"
-                            sx={{
-                                fontSize: "16px",
-                                fontWeight: 600,
-                                color: "#000",
-                                mb: "10px"
-                            }}
-                        >
-                            Group
-                        </Typography>
-                        <AllSelectFullWidth
-                            chageValueFunction={val => setData(prev => {
-                                return {
-                                    ...prev,
-                                    group: val
-                                }
-                            })}
-                            selectOptions={groups}
-                        />
-                    </ModalSelectWrapper>
-                    <ModalSelectWrapper>
-                        <Typography
-                            id="keep-mounted-modal-title"
-                            variant="h6"
-                            component="h4"
-                            sx={{
-                                fontSize: "16px",
-                                fontWeight: 600,
-                                color: "#000",
-                                mb: "10px"
-                            }}
-                        >
-                            Weekday
-                        </Typography>
-                            {getDay(day)}
-                        {/* <AllSelectFullWidth
+              Weekday
+            </Typography>
+            {getDay(day)}
+            {/* <AllSelectFullWidth
                             chageValueFunction={val => setData(prev => {
                                 return {
                                     ...prev,
@@ -202,23 +204,23 @@ export default function DayTable({ oneday, groups, day, para, room }) {
                             })}
                             selectOptions={weekDays}
                         /> */}
-                    </ModalSelectWrapper>
-                    <ModalSelectWrapper>
-                        <Typography
-                            id="keep-mounted-modal-title"
-                            variant="h6"
-                            component="h4"
-                            sx={{
-                                fontSize: "16px",
-                                fontWeight: 600,
-                                color: "#000",
-                                mb: "10px"
-                            }}
-                        >
-                            Para
-                        </Typography>
-                            {para}
-                        {/* <AllSelectFullWidth
+          </ModalSelectWrapper>
+          <ModalSelectWrapper>
+            <Typography
+              id="keep-mounted-modal-title"
+              variant="h6"
+              component="h4"
+              sx={{
+                fontSize: "16px",
+                fontWeight: 600,
+                color: "#000",
+                mb: "10px"
+              }}
+            >
+              Para
+            </Typography>
+            {para}
+            {/* <AllSelectFullWidth
                             chageValueFunction={val => setData(prev => {
                                 return {
                                     ...prev,
@@ -227,23 +229,23 @@ export default function DayTable({ oneday, groups, day, para, room }) {
                             })}
                             selectOptions={paraList}
                         /> */}
-                    </ModalSelectWrapper>
-                    <ModalSelectWrapper>
-                        <Typography
-                            id="keep-mounted-modal-title"
-                            variant="h6"
-                            component="h4"
-                            sx={{
-                                fontSize: "16px",
-                                fontWeight: 600,
-                                color: "#000",
-                                mb: "10px"
-                            }}
-                        >
-                            Room
-                        </Typography>
-                            {room?.name}
-                        {/* <AllSelectFullWidth
+          </ModalSelectWrapper>
+          <ModalSelectWrapper>
+            <Typography
+              id="keep-mounted-modal-title"
+              variant="h6"
+              component="h4"
+              sx={{
+                fontSize: "16px",
+                fontWeight: 600,
+                color: "#000",
+                mb: "10px"
+              }}
+            >
+              Room
+            </Typography>
+            {room?.name}
+            {/* <AllSelectFullWidth
                             chageValueFunction={val => setData(prev => {
                                 return {
                                     ...prev,
@@ -252,47 +254,47 @@ export default function DayTable({ oneday, groups, day, para, room }) {
                             })}
                             selectOptions={roomList}
                         /> */}
-                    </ModalSelectWrapper>
-                    <ModalSelectWrapper>
-                        <Typography
-                            id="keep-mounted-modal-title"
-                            variant="h6"
-                            component="h4"
-                            sx={{
-                                fontSize: "16px",
-                                fontWeight: 600,
-                                color: "#000",
-                                mb: "10px"
-                            }}
-                        >
-                            Types
-                        </Typography>
-                        <AllSelectFullWidth
-                            chageValueFunction={val => setData(prev => {
-                                return {
-                                    ...prev,
-                                    types: val
-                                }
-                            })}
-                            selectOptions={lessonTypes}
-                        />
-                    </ModalSelectWrapper>
-                </DialogContent>
-                <DialogActions>
-                    <Button autoFocus onClick={handleSubmit}>
-                        Save changes
-                    </Button>
-                </DialogActions>
-            </BootstrapDialog>
-        </>
-    )
+          </ModalSelectWrapper>
+          <ModalSelectWrapper>
+            <Typography
+              id="keep-mounted-modal-title"
+              variant="h6"
+              component="h4"
+              sx={{
+                fontSize: "16px",
+                fontWeight: 600,
+                color: "#000",
+                mb: "10px"
+              }}
+            >
+              Types
+            </Typography>
+            <AllSelectFullWidth
+              chageValueFunction={val => setData(prev => {
+                return {
+                  ...prev,
+                  types: val
+                }
+              })}
+              selectOptions={lessonTypes}
+            />
+          </ModalSelectWrapper>
+        </DialogContent>
+        <DialogActions>
+          <Button autoFocus onClick={handleSubmit}>
+            Save changes
+          </Button>
+        </DialogActions>
+      </BootstrapDialog>
+    </>
+  )
 }
 
 const getDay = (day_number) => {
-    if(day_number == 1) return "monday"
-    if(day_number == 2) return "tuesday"
-    if(day_number == 3) return "wednesday"
-    if(day_number == 4) return "thursday"
-    if(day_number == 5) return "friday"
-    if(day_number == 6) return "saturday"
+  if (day_number == 1) return "monday"
+  if (day_number == 2) return "tuesday"
+  if (day_number == 3) return "wednesday"
+  if (day_number == 4) return "thursday"
+  if (day_number == 5) return "friday"
+  if (day_number == 6) return "saturday"
 }
