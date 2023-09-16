e1ab4b984482312b5ee100927276f3c54e7c829b
JaMeS-18-18 <chitak1818@gmail.com>
2023-09-16 21:39:31 +0500

    time table edit

--- src/components/TeacherStatistic/index.jsx
+++ src/components/TeacherStatistic/index.jsx
@@ -35,12 +35,12 @@ export default function TeacherStatistic() {
           </WrapperHeaderRight>
         </TeacherStatisticWrapperHeader>
         <Link to={'/teacher/sciences'}>
-        <TeacherStatisticWrapperBottom>
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle" viewBox="0 0 16 16">
-            <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
-          </svg>
-          <p>Davomat jurnali</p>
-        </TeacherStatisticWrapperBottom>
+          <TeacherStatisticWrapperBottom>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
+            </svg>
+            <p>Davomat jurnali</p>
+          </TeacherStatisticWrapperBottom>
         </Link>
       </TeacherStatisticWrapper>
 
@@ -48,7 +48,7 @@ export default function TeacherStatistic() {
       <TeacherStatisticWrapper>
         <TeacherStatisticWrapperHeader>
           <WrapperHeaderLeft>
-            <h2>{ScienceListCount.lesson_hours}</h2>
+            <h2>{ScienceListCount.studied_hours}/{ScienceListCount.lesson_hours}</h2>
             <p>Mening dars jadvalim</p>
           </WrapperHeaderLeft>
           <WrapperHeaderRight>
@@ -58,12 +58,12 @@ export default function TeacherStatistic() {
           </WrapperHeaderRight>
         </TeacherStatisticWrapperHeader>
         <Link to={'/teacher/classschedule'}>
-        <TeacherStatisticWrapperBottom>
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle" viewBox="0 0 16 16">
-            <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
-          </svg>
-          <p>Mening dars jadvalim</p>
-        </TeacherStatisticWrapperBottom>
+          <TeacherStatisticWrapperBottom>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
+            </svg>
+            <p>Mening dars jadvalim</p>
+          </TeacherStatisticWrapperBottom>
         </Link>
       </TeacherStatisticWrapper>
 
@@ -91,6 +91,47 @@ export default function TeacherStatistic() {
             <p>Darslar ro'yxati</p>
           </TeacherStatisticWrapperBottom>
         </Link>
+      </TeacherStatisticWrapper>
+
+      <TeacherStatisticWrapper>
+        <TeacherStatisticWrapperHeader>
+          <WrapperHeaderLeft>
+            <h2>{ScienceListCount.controls}</h2>
+            <p>Oraliq nazorat</p>
+          </WrapperHeaderLeft>
+          <WrapperHeaderRight>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-hourglass-split" viewBox="0 0 16 16">
+              <path d="M2.5 15a.5.5 0 1 1 0-1h1v-1a4.5 4.5 0 0 1 2.557-4.06c.29-.139.443-.377.443-.59v-.7c0-.213-.154-.451-.443-.59A4.5 4.5 0 0 1 3.5 3V2h-1a.5.5 0 0 1 0-1h11a.5.5 0 0 1 0 1h-1v1a4.5 4.5 0 0 1-2.557 4.06c-.29.139-.443.377-.443.59v.7c0 .213.154.451.443.59A4.5 4.5 0 0 1 12.5 13v1h1a.5.5 0 0 1 0 1h-11zm2-13v1c0 .537.12 1.045.337 1.5h6.326c.216-.455.337-.963.337-1.5V2h-7zm3 6.35c0 .701-.478 1.236-1.011 1.492A3.5 3.5 0 0 0 4.5 13s.866-1.299 3-1.48V8.35zm1 0v3.17c2.134.181 3 1.48 3 1.48a3.5 3.5 0 0 0-1.989-3.158C8.978 9.586 8.5 9.052 8.5 8.351z" />
+            </svg>
+          </WrapperHeaderRight>
+        </TeacherStatisticWrapperHeader>
+          <TeacherStatisticWrapperBottom>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
+            </svg>
+            <p>Oraliq nazorat</p>
+          </TeacherStatisticWrapperBottom>
+      </TeacherStatisticWrapper>
+
+
+      <TeacherStatisticWrapper>
+        <TeacherStatisticWrapperHeader>
+          <WrapperHeaderLeft>
+            <h2>{ScienceListCount.end_controls}</h2>
+            <p>Yakuniy nazorat</p>
+          </WrapperHeaderLeft>
+          <WrapperHeaderRight>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-hourglass-bottom" viewBox="0 0 16 16">
+              <path d="M2 1.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-1v1a4.5 4.5 0 0 1-2.557 4.06c-.29.139-.443.377-.443.59v.7c0 .213.154.451.443.59A4.5 4.5 0 0 1 12.5 13v1h1a.5.5 0 0 1 0 1h-11a.5.5 0 1 1 0-1h1v-1a4.5 4.5 0 0 1 2.557-4.06c.29-.139.443-.377.443-.59v-.7c0-.213-.154-.451-.443-.59A4.5 4.5 0 0 1 3.5 3V2h-1a.5.5 0 0 1-.5-.5zm2.5.5v1a3.5 3.5 0 0 0 1.989 3.158c.533.256 1.011.791 1.011 1.491v.702s.18.149.5.149.5-.15.5-.15v-.7c0-.701.478-1.236 1.011-1.492A3.5 3.5 0 0 0 11.5 3V2h-7z" />
+            </svg>
+          </WrapperHeaderRight>
+        </TeacherStatisticWrapperHeader>
+          <TeacherStatisticWrapperBottom>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
+            </svg>
+            <p>Yakuniy nazorat</p>
+          </TeacherStatisticWrapperBottom>
       </TeacherStatisticWrapper>
     </ContentWrapper>
   )
