a6e19ece7a36b917e9e59e617f0af9120aaa696f
shohruzbekcoder <shohruzbekcoder007@gamil.com>
2023-09-17 18:16:05 +0500

    17.09.2023 18:16

--- src/utils/API_urls.js
+++ src/utils/API_urls.js
@@ -1,4 +1,4 @@
-export const host = "https://e4d6-178-218-200-199.ngrok-free.app"
+// export const host = "https://e4d6-178-218-200-199.ngrok-free.app"
 // export const host = "http://oqdevpy.jprq.live"
 // export const host = "http://husanibragimov.jprq.live"
 // export const host = "https://api.tift.uz"
