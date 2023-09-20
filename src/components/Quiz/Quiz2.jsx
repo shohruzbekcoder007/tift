c5c4bcbb49baab82cee4ed814288fb0587d926e9
shohruzbekcoder <shohruzbekcoder007@gmail.com>
2023-09-20 17:21:21 +0500

    vaqt bo'ldi 17:21

--- src/components/Quiz/Quiz2.jsx
+++ src/components/Quiz/Quiz2.jsx
@@ -18,7 +18,7 @@ const styles = theme => ({
     paddingTop: 16,
     paddingBottom: 16,
     marginTop: theme.spacing.unit * 3,
-    minWidth: "800px",
+    minWidth: "1000px",
     margin: "10px auto"
   },
   button: {
@@ -50,19 +50,6 @@ function PaperSheet(props) {
   const [finishedTest, setFinishedTest] = useState(false)
   const [graduate, setGraduate] = useState(0)
   const [nextTime, setNextTime] = useState(true)
-
-  const getSelectedValue = () => {
-    const isLargeNumber = (element) => element.question_id == quiz[current].id;
-    const fInd = answers.findIndex(isLargeNumber);
-    let selectedquestion = -1;
-    console.log(fInd)
-    if (fInd > 0) {
-      selectedquestion = answers[fInd].aswer_id
-      setSelectedValue(answers[fInd].aswer_id)
-    }
-    console.log(selectedquestion, answers, quiz[current].id)
-    //shu yerdan tugirlanadi orqaga qaytishi
-  }
 
   useEffect(() => {
     getQuizs(`${student_test_detail}${testId}`, response => {
@@ -100,12 +87,10 @@ function PaperSheet(props) {
 
   const moveNext = () => {
     setCurrent(prev => prev + 1)
-    getSelectedValue()
   }
 
   const movePrevious = () => {
     setCurrent(prev => prev - 1)
-    getSelectedValue()
   }
 
   const revealCorrect = () => {
@@ -197,7 +182,7 @@ function PaperSheet(props) {
               <div key={index} style={{ marginTop: "5px" }}
               >
                 <Radio
-                  checked={opt.id == selectedValue}
+                  checked={answers.find(elem => elem.question_id == quiz[current].id)?.aswer_id == opt.id}
                   onChange={handleChange}
                   value={opt.id}
                   name={`${quiz[current].id}`}
@@ -208,31 +193,51 @@ function PaperSheet(props) {
           })}
           <div className={props.classes.footer}>
             {/* {komentni uchirmang} */}
-            {/* <Button onClick={revealCorrect} variant="raised" color="secondary">
-            Yakunlash
+            <Button
+            variant="contained"
+            color='secondary'
+            onClick={() => {
+              revealCorrect()
+              navigate(-1)
+            }}
+            sx={{
+              borderRadius: "10px",
+              // textTransform: "capitalize",
+              boxShadow: "none",
+              padding: "14px",
+              marginRight: "20px"
+            }}
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+              <path d="M19 10.9998H9L12.29 7.70982C12.3837 7.61685 12.4581 7.50625 12.5089 7.38439C12.5597 7.26253 12.5858 7.13183 12.5858 6.99982C12.5858 6.8678 12.5597 6.7371 12.5089 6.61524C12.4581 6.49338 12.3837 6.38278 12.29 6.28982C12.1026 6.10356 11.8492 5.99902 11.585 5.99902C11.3208 5.99902 11.0674 6.10356 10.88 6.28982L6.59 10.5898C6.21441 10.9632 6.00223 11.4702 6 11.9998C6.00487 12.5259 6.21684 13.0289 6.59 13.3998L10.88 17.6998C10.9732 17.7924 11.0838 17.8657 11.2054 17.9156C11.3269 17.9654 11.4571 17.9908 11.5885 17.9904C11.7199 17.9899 11.8499 17.9636 11.9712 17.9129C12.0924 17.8621 12.2024 17.7881 12.295 17.6948C12.3876 17.6016 12.4609 17.491 12.5107 17.3694C12.5606 17.2479 12.586 17.1177 12.5856 16.9863C12.5851 16.8549 12.5588 16.7249 12.508 16.6037C12.4573 16.4824 12.3832 16.3724 12.29 16.2798L9 12.9998H19C19.2652 12.9998 19.5196 12.8945 19.7071 12.7069C19.8946 12.5194 20 12.265 20 11.9998C20 11.7346 19.8946 11.4802 19.7071 11.2927C19.5196 11.1052 19.2652 10.9998 19 10.9998Z" fill="black" />
+            </svg>
+            Yakunlash va vaziflarga qaytish
           </Button>
-          {(current + 1 < quiz.length) ? (<Button onClick={moveNext} variant="raised" color="primary" style={{ float: "right" }}>
+            <Button onClick={revealCorrect} variant="contained" color="primary" sx={{padding: '14px'}}>
+            Yakunlash va natijani ko'rish
+          </Button>
+          {(current + 1 < quiz.length) ? (<Button onClick={moveNext} variant="contained" color="primary" style={{ float: "right" }} sx={{padding: '14px'}}>
             Keyingi
-          </Button>) : (<Button onClick={moveNext} disabled variant="raised" color="primary" style={{ float: "right" }}>
+          </Button>) : (<Button onClick={moveNext} disabled variant="contained" color="primary" style={{ float: "right" }} sx={{padding: '14px'}}>
             Keyingi
           </Button>)}
 
-          {(current == 0) ? (<Button onClick={movePrevious} disabled variant="raised" color="primary" style={{ float: "right", marginRight: "50px" }}>
+          {(current == 0) ? (<Button onClick={movePrevious} disabled variant="contained" color="primary" style={{ float: "right", marginRight: "50px" }} sx={{padding: '14px'}}>
             Oldingi
-          </Button>) : (<Button onClick={movePrevious} variant="raised" color="primary" style={{ float: "right", marginRight: "50px" }}>
+          </Button>) : (<Button onClick={movePrevious} variant="contained" color="primary" style={{ float: "right", marginRight: "50px" }} sx={{padding: '14px'}}>
             Oldingi
-          </Button>)} */}
-            {
+          </Button>)}
+            {/* {
               (!finishedTest) ? <>
                 {
-                  (current + 1 < quiz.length) ? (<Button onClick={moveNext} variant="raised" color="primary" style={{ float: "right" }}>
+                  (current + 1 < quiz.length) ? (<Button onClick={moveNext} variant="contained" color="primary" style={{ float: "right" }}>
                     Keyingi
-                  </Button>) : (<Button onClick={revealCorrect} variant="raised" color="secondary">
-                    Yakunlash
+                  </Button>) : (<Button onClick={revealCorrect} variant="contained" color="secondary">
+                    Yakunlash va natijani ko'rish
                   </Button>)
                 }
               </> : <></>
-            }
+            } */}
           </div>
         </>}
       </Paper>
