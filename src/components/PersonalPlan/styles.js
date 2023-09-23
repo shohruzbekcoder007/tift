b90992c4f5e8eb36545d11e6432e6a9df5b26b34
JaMeS-18-18 <chitak1818@gmail.com>
2023-09-23 14:27:02 +0500

    admin student edited

--- src/components/PersonalPlan/styles.js
+++ src/components/PersonalPlan/styles.js
@@ -11,6 +11,7 @@ export const PersonalPlanPaper = styled(Paper)`
     width: calc(50% - 18px);
     padding: 20px;
     border-radius: 10px;
+    height: 400px;
     @media (max-width: 740px){
         width: 100%;
     }
---
+++ src/dictionary/language.js
@@ -0,0 +1,20 @@
+export default [
+  {
+      value: "uz",
+      uz: "Uzbek",
+      ru: "bachelor ru",
+      eng: "bachelor eng"
+  },
+  {
+      value: "ru",
+      uz: "Russian",
+      ru: "Rus",
+      eng: "master eng"
+  },
+  {
+    value: "en",
+    uz: "English",
+    ru: "master ru",
+    eng: "master eng"
+}
+]
